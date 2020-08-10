10a0572c99e16636b2f28651cc7c959a8bcfb30a
Julie Nginn <nginn.julie@gmail.com>
2020-08-10 21:52:50 +0200

    Feat: Add characters selection

---
+++ Assets/Scripts/Managers/CharSelectorManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelectorManager : MonoBehaviour
+{
+    public GameObject[] characters;
+
+    [SerializeField]
+    private GameObject nextButton;
+
+    [SerializeField]
+    private GameObject prevButton;
+
+    private int characterIndex = 0;
+
+    private void Awake()
+    {
+        characters[characterIndex].SetActive(true);
+    }
+
+    private void Start()
+    {
+        if (characterIndex < characters.Length)
+        {
+            nextButton.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (characterIndex + 1 == characters.Length)
+        {
+            nextButton.SetActive(false);
+        }
+
+        if (characterIndex + 1 < characters.Length)
+        {
+            nextButton.SetActive(true);
+        }
+
+        if (characterIndex < characters.Length && characterIndex != 0)
+        {
+            prevButton.SetActive(true);
+        }
+
+        if (characterIndex == 0)
+        {
+            prevButton.SetActive(false);
+        }
+
+        foreach (GameObject character in characters)
+        {
+            if (character.name == characters[characterIndex].name)
+            {
+                characters[characterIndex].SetActive(true);
+            }
+            else
+            {
+                character.SetActive(false);
+            }
+        }
+    }
+
+    public void NextCharacter()
+    {
+        characterIndex++;
+    }
+
+    public void PreviousCharacter()
+    {
+        characterIndex--;
+    }
+}
