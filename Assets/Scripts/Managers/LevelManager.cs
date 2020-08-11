fb8a14a41f660ab48d756f5df0f4b0fbe11c96b4
Julie Nginn <nginn.julie@gmail.com>
2020-08-11 18:55:07 +0200

    Feat: Add level indicator

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text levelNumber;
+
+    private void Start()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
+        levelNumber.text = string.Format("LEVEL {0}", sceneIndex);
+    }
+}
