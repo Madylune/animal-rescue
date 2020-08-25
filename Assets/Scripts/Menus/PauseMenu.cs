cadce6f25bcfc568841880c882ceb5968bf23c2d
Julie Nginn <nginn.julie@gmail.com>
2020-08-25 16:14:59 +0200

    Feat: Adding pause menu

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f; // Freeze time and game
+        isPaused = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void OpenSettings()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
